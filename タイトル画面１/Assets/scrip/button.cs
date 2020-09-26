01cedfd6cf9e83d5c9b3281579242ed3251bcd91
402-26\402user <>
2020-09-26 16:06:01 +0900

    Title+

---
+++ タイトル画面１/Assets/scrip/button.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void click() {
+        Debug.Log("ウィンドウ呼び出し");
+    }
+}
---
+++ タイトル画面１/Assets/scrip/zukanscene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class zukanscene : MonoBehaviour
+{
+    public void OnclickStartButton()
+    {
+        SceneManager.LoadScene("zukann");//シーン名図鑑
+    }
+}
